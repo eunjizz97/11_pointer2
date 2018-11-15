c031c2e9f9a5db1e9ac295986d794443b3171668
lee eunji <eunjizz97@sookmyung.ac.kr>
2018-11-15 15:59:41 +0900

    함수 포인터

--- main.c
+++ main.c
@@ -2,18 +2,57 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-char *proverb = "All that gliters is not gold.";
-
-void setPointer(char **q)
+int plus(int N1, int N2)
 {
-	*q = proverb;
+	return (N1+N2);
+}
+
+int minus(int N1, int N2)
+{
+	return (N1-N2);
+}
+
+int multi(int N1, int N2)
+{
+	return (N1*N2);
+}
+
+int divfun(int N1, int N2)
+{
+	return (N1/N2);
 }
 
 int main(int argc, char *argv[]) {
 	
-	char *p ="zzz";
-	setPointer(&p);
-	printf("%s \n", p);
+	int n1, n2;
+	char c;
+	int (*pf)(int, int);
+	int result;
+	
+	printf("input calculation : ");
+	scanf("%d %c %d", &n1, &c, &n2);
+	
+	switch(c)
+	{
+		case '+':
+			pf=plus;
+			break;
+			
+		case '-':
+			pf=minus;
+			break;
+			
+		case '*':
+			pf=multi;
+			break;
+			
+		case '/':
+			pf=divfun;
+			break;
+	}
+	
+	result = pf(n1,n2);
+	printf("result = %d", result);
 	
 	return 0;
 }
